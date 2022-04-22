86746d257323987d15bac703cf459f79a606ef1d
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-22 13:26:38 +0530

    updating

--- db/mysql/data.tf
+++ db/mysql/data.tf
@@ -13,13 +13,13 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-data "aws_secretsmanager_secret" "dev" {
-  #name = "secrets/roboshop/${var.ENV}"
+data "aws_secretsmanager_secret" "secret" {
+  name = "secrets/dev/${var.ENV}"
   #name=var.ENV
-  name="dev"
+  #name="dev"
 }
 
 data "aws_secretsmanager_secret_version" "latest" {
-  secret_id = data.aws_secretsmanager_secret.dev.id
+  secret_id = data.aws_secretsmanager_secret.secret.id
 }
 
