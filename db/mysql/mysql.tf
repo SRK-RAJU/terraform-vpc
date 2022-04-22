c268593d7f404213fb9ca4caaef15f3cf42e1eef
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-22 11:48:45 +0530

    updating

--- db/mysql/mysql.tf
+++ db/mysql/mysql.tf
@@ -1,6 +1,6 @@
 resource "aws_db_instance" "default" {
 
-  identifier = "mysql-${var.ENV}"
+  identifier = "roboshop-mysql-${var.ENV}"
   allocated_storage      = 10
   engine                 = "mysql"
   engine_version         = "5.7"
