b265b3a3cb27d9b2751f6b9e58a7a42ff11f8965
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-23 12:08:01 +0530

    updating

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -7,11 +7,11 @@ locals {
 
 resource "aws_db_instance" "mysql" {
   allocated_storage      = 10
-  identifier             = "mysqldb-${var.ENV}"
+  identifier             = "mysql-${var.ENV}"
   engine                 = "mysql"
   engine_version         = "5.7"
   instance_class         = "db.t3.micro"
-  name                   = "admin"
+  name                   = "mysqldb.ctfkbpezmfa1.us-east-1.rds.amazonaws.com"
   username               = local.rds_user
   password               = local.rds_pass
   parameter_group_name   = aws_db_parameter_group.pg.name
