118e99ba657e477f7949cffeb48fdcd7c3c83cd3
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-24 11:37:47 +0530

    updating

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "mysql" {
   allocated_storage      = 10
   #identifier             = "mysqldb-${var.ENV}"
-  identifier             = "mysqldb"
+  identifier             = "mysql"
   engine                 = "mysql"
   engine_version         = "5.7"
   instance_class         = "db.t3.micro"
