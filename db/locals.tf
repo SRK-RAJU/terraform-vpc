ae02f97dcd82937552a4af7185b28142137c89d9
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-24 11:21:24 +0530

    updating

--- db/locals.tf
+++ db/locals.tf
@@ -2,5 +2,5 @@ locals {
   rds_user = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_USER"]
   rds_pass = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_PASS"]
   DEFAULT_VPC_CIDR = split(",", data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR)
-  ALL_CIDR         = concat["(data.terraform_remote_state.vpc.outputs.VPC_CIDR, local.DEFAULT_VPC_CIDR)"]
+  ALL_CIDR         = concat["data.terraform_remote_state.vpc.outputs.VPC_CIDR, local.DEFAULT_VPC_CIDR"]
 }
