9e491dff7b3ccbcfb0a4f816db8b56f688417123
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-24 14:04:01 +0530

    updating

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -10,6 +10,7 @@ resource "aws_subnet" "public-subnets" {
 
 resource "aws_subnet" "private-subnets" {
   count             = var.PRIVATE_SUBNETS
+  #count             = length(var.PRIVATE_SUBNET_CIDR)
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(var.VPC_CIDR, 3, count.index + 2)
   availability_zone = element(var.AZ, count.index)
