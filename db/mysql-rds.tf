98f723364aa6390663c3721c2f06d36cf08e12fa
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-24 10:54:29 +0530

    updating

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -7,7 +7,8 @@ locals {
 
 resource "aws_db_instance" "mysql" {
   allocated_storage      = 10
-  identifier             = "mysqldb-${var.ENV}"
+  #identifier             = "mysqldb-${var.ENV}"
+  identifier             = "mysqldb"
   engine                 = "mysql"
   engine_version         = "5.7"
   instance_class         = "db.t3.micro"
--- db/vars.tf
+++ db/vars.tf
@@ -2,3 +2,6 @@ variable "ENV" {}
 variable "MONGODB_INSTANCE_TYPE" {}
 variable "RABBITMQ_INSTANCE_TYPE" {}
 variable "ALL_CIDR" {}
+variable "PRIVATE_SUBNETS_IDS" {}
+variable "INTERNAL_HOSTEDZONE_ID" {}
+variable "VPC_ID" {}
