db710dd2205ae35283cc269f4b986bc4ab03abe3
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-25 09:47:36 +0530

    updating

--- vpc/vars.tf
+++ vpc/vars.tf
@@ -7,5 +7,8 @@ variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_ROUTE_TABLE_ID" {}
 variable "DEFAULT_VPC_CIDR" {}
 variable "PRIVATE_HOSTED_ID" {}
+variable "PUBLIC_SUBNETS_IDS" {}
+variable "PRIVATE_SUBNETS_IDS" {}
+
 
 
