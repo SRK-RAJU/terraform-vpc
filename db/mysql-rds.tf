0dec4d79e7641df813dc3188f01a924a40aa6462
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-26 21:13:32 +0530

    updating

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -1,8 +1,8 @@
 locals {
   rds_user = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_USER"]
  rds_pass = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["RDS_PASS"]
-  DEFAULT_VPC_CIDR = split(",", data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR)
-  ALL_CIDR         = concat(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR, local.DEFAULT_VPC_CIDR)
+  D_VPC_CIDR = split(",", data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR)
+  ALL_CIDR         = concat(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR, local.D_VPC_CIDR)
  # rds_user="admin"
 #  rds_pass="admin123"
 }
