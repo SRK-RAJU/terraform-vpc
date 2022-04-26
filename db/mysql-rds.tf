0caec4b8d47f3f086bf04395eafbcd03595a7942
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-26 21:21:27 +0530

    updating

--- db/mysql-rds.tf
+++ db/mysql-rds.tf
@@ -62,14 +62,26 @@ resource "aws_security_group" "mysql" {
   }
     ]
 
-  egress = [ {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-]
+  #egress = [ {
+   # from_port        = 0
+   # to_port          = 0
+   # protocol         = "-1"
+   # cidr_blocks      = ["0.0.0.0/0"]
+   # ipv6_cidr_blocks = ["::/0"]
+  #}
+    egress = [
+      {
+        description      = "egress"
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+        prefix_list_ids  = []
+        security_groups  = []
+        self             = false
+      }
+    ]
 
 
     tags = {
