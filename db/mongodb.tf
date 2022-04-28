08b1081d66987532c3b5400c3c0a6c4621f688ee
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-28 22:00:58 +0530

    updating

--- db/mongodb.tf
+++ db/mongodb.tf
@@ -1,3 +1,30 @@
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier = "roboshop-${var.ENV}"
+  engine             = "docdb"
+  master_username    = "admin1"
+  master_password    = "roboshop1"
+  ## This is just for lab purpose
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_docdb_subnet_group.docdb.name
+}
+
+resource "aws_docdb_subnet_group" "docdb" {
+  name       = "roboshop-${var.ENV}"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
+
+  tags = {
+    Name = "roboshop-${var.ENV}"
+  }
+}
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "roboshop-${var.ENV}"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = "db.t3.medium"
+}
+
+
 resource "aws_spot_instance_request" "mongodb" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.MONGODB_INSTANCE_TYPE
