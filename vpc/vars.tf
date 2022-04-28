6f6542cb3d1c0c11088ed376c903c2e4c0cf3ff5
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-28 18:48:16 +0530

    updating

--- vpc/vars.tf
+++ vpc/vars.tf
@@ -10,9 +10,9 @@ variable "DEFAULT_VPC_CIDR" {}
 variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
 
-variable "PRIVATE_HOSTED_ZONE_ID" {}
-variable "PUBLIC_HOSTED_ZONE_ID" {}
-variable "PRIVATE_HOSTED_ZONE_NAME" {}
-variable "PUBLIC_HOSTED_ZONE_NAME" {}
+#variable "PRIVATE_HOSTED_ZONE_ID" {}
+#variable "PUBLIC_HOSTED_ZONE_ID" {}
+#variable "PRIVATE_HOSTED_ZONE_NAME" {}
+#variable "PUBLIC_HOSTED_ZONE_NAME" {}
 variable "VPC_CIDR_ADDON" {}
 
