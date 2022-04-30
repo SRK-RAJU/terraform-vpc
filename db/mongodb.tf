baaaec525d792b3e9f8ee9b2d188701c06106461
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-30 11:37:43 +0530

    updating

--- db/mongodb.tf
+++ db/mongodb.tf
@@ -120,9 +120,9 @@ systemctl reload mongodb
 systemctl restart mongodb
 systemctl enable mongodb
 
-
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
 unzip -o mongodb.zip
 cd mongodb-main
 mongo --ssl --sslCAFile /home/centos/rds-combined-ca-bundle.pem --host ${aws_docdb_cluster.docdb.endpoint} --username admin1 --password roboshop1 < catalogue.js
