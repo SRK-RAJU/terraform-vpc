c93d036b2098fa36897d541b1df1481e953cb403
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-30 11:34:18 +0530

    updating

--- db/mongodb.tf
+++ db/mongodb.tf
@@ -125,8 +125,8 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongo
 cd /tmp
 unzip -o mongodb.zip
 cd mongodb-main
-mongo --ssl --sslCAFile /home/roboshop/rds-combined-ca-bundle.pem --host ${aws_docdb_cluster.docdb.endpoint} --username admin1 --password roboshop1 < catalogue.js
-mongo --ssl --sslCAFile /home/roboshop/rds-combined-ca-bundle.pem --host ${aws_docdb_cluster.docdb.endpoint} --username admin1 --password roboshop1 < users.js
+mongo --ssl --sslCAFile /home/centos/rds-combined-ca-bundle.pem --host ${aws_docdb_cluster.docdb.endpoint} --username admin1 --password roboshop1 < catalogue.js
+mongo --ssl --sslCAFile /home/centos/rds-combined-ca-bundle.pem --host ${aws_docdb_cluster.docdb.endpoint} --username admin1 --password roboshop1 < users.js
 EOF
   }
 }
