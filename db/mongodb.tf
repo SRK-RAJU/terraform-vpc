2c26afe6b8087c283e12995841b7fbd3abfd9fd3
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-30 09:48:20 +0530

    updating

--- db/mongodb.tf
+++ db/mongodb.tf
@@ -104,7 +104,8 @@ resource "aws_security_group" "allow-mongodb" {
 resource "null_resource" "db-deploy" {
   provisioner "remote-exec" {
     connection {
-     # host     = aws_spot_instance_request.mongodb.private_ip
+       #host     = aws_spot_instance_request.mongodb.private_ip
+      host     = aws_docdb_cluster_instance.cluster_instances
       user     = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_USER"]
       password = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_PASS"]
 
