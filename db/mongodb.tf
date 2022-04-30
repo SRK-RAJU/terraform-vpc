055ba71b66c6a2a22e8d1069979fc81793e1f5cf
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-30 11:15:18 +0530

    updating

--- db/mongodb.tf
+++ db/mongodb.tf
@@ -114,8 +114,8 @@ resource "aws_security_group" "allow-mongodb" {
 resource "null_resource" "schema-mongodb" {
   provisioner "local-exec" {
     command = <<EOF
-cd /tmp
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+cd /tmp
 unzip -o mongodb.zip
 cd mongodb-main
 mongo --ssl --sslCAFile /home/roboshop/rds-combined-ca-bundle.pem --host ${aws_docdb_cluster.docdb.endpoint} --username admin1 --password roboshop1 < catalogue.js
