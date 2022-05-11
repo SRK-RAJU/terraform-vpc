6c5c21bc639b34d34657a915e9bb2781556df358
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-05-11 10:42:06 +0530

    updating

--- db/locals.tf
+++ db/locals.tf
@@ -7,4 +7,7 @@ locals {
 
   #ssh_user         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_USER"]
   #sh_pass         = jsondecode(data.aws_secretsmanager_secret_version.secrets-version.secret_string)["SSH_PASS"]
+
+  D_VPC_CIDR = split(",", data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR)
+  ALL_CIDR         = concat(data.terraform_remote_state.vpc.outputs.ALL_VPC_CIDR, local.D_VPC_CIDR)
 }
